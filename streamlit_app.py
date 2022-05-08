5a46de516e0043c6b9e438b8221d6ac0523c330b
Albert Eksteen <albert.eksteen@gmail.com>
2022-05-08 14:45:04 +0200

    Update streamlit_app.py
    added the functionality to search your own fruit on the api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,12 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = stramlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 #new section to display fruityvice api response
 import requests
-fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_responce.json())
